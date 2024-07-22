b6e14b92a009b0139c6ae62b6dd6746b127b1ca4
Ahn Seung Seop <maketheworldwise@squares.ai>
2024-07-22 09:32:22 +0900

    test

---
+++ test.ts
@@ -0,0 +1,7 @@
+/**
+ * TODO Should we reinvent the wheel here?
+ * We already have a good one. But could it be even rounder?
+ */
+function getWheel() {
+    // Returns you this pizza like thing
+}
